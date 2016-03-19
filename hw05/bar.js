9e18c07c5625d6df325cede6fa956e8d7d2de887
Sally Bao <sallybao29@gmail.com>
2016-03-19 13:17:42 -0400

    graphs with d3

---
+++ hw05/bar.js
@@ -0,0 +1,32 @@
+console.log("loaded");
+
+var data = [4, 8, 15, 16, 23, 42];
+
+/*
+var chart = d3.select(".chart");
+
+var bar = chart.selectAll("div");
+var barUpdate = bar.data(data);
+var barEnter = barUpdate.enter().append("div");
+
+barEnter.style("width", function(d){
+    return d * 10 + "px";});
+
+barEnter.text(function(d){
+    return "" + d;});
+*/
+
+//To scale bars:
+var foo = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+
+d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d){ return foo(d) + "px"; })
+    .text(function(d){ return d; });
+
+
--- sandbox/ipsum.js
+++ sandbox/ipsum.js
@@ -3,3 +3,7 @@ console.log("Loaded");
 d3.selectAll("p")
     .data([14, 16, 18])
     .style("font-size", function(d){return d + "px";});
+
+d3.selectAll("p").style("color", function(){
+    return "hsl(" + Math.random() * 360 + ", 100%, 50%)";
+});
